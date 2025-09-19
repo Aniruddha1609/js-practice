29bd7575d08d281338082a4d1db91793656fb4d8
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-19 13:38:55 +0530

    Create fibonacci_exp1.js for rough work for fibonacci series

---
+++ fibonacci/fibonacci_exp1.js
@@ -0,0 +1,38 @@
+let inputForLimit = 1 ;
+let fibonacciValue = 0;
+let secondAddingValue = 1
+let firstAddingValue = 0;
+
+if(inputForLimit === 0){
+   console.log("The fibonacci number for ",inputForLimit,"is",0);
+}
+
+else if(inputForLimit === 1){
+    console.log("The fibonacci number for ",inputForLimit,"is",1);
+}
+
+else{
+  fibonacciValue = secondAddingValue + firstAddingValue; 
+  firstAddingValue = secondAddingValue;
+  secondAddingValue = fibonacciValue;
+
+  fibonacciValue = secondAddingValue + firstAddingValue;
+  firstAddingValue = secondAddingValue;
+  secondAddingValue = fibonacciValue ;
+
+  fibonacciValue = secondAddingValue + firstAddingValue;
+  firstAddingValue = secondAddingValue;
+  secondAddingValue = fibonacciValue ;
+
+  fibonacciValue = secondAddingValue + firstAddingValue;
+  firstAddingValue = secondAddingValue;
+  secondAddingValue = fibonacciValue ;
+
+//   fibonacciValue = addingValue + firstValue;
+//   firstValue = addingValue;
+//   addingValue = fibonacciValue ;
+  
+  console.log("The fibonacci number for ",inputForLimit,"is",fibonacciValue);
+}
+
+
