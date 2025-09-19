67f31cf9fdce7994453388406c547cdda99af148
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-19 12:38:26 +0530

    Creating the main file of factorial.js

---
+++ factorial/factorial.js
@@ -0,0 +1,11 @@
+const input = 5;
+let factorial= 1;
+let multiplier = 1;
+
+for(multiplier ; multiplier <= input ; multiplier++){
+  factorial = factorial * multiplier;
+}
+console.log("Factorial of input",input,"is",factorial);
+    
+
+
