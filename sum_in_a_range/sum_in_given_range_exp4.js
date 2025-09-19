08f0bf385c81a28c1dc79446ab987a90c9ced4a0
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-19 13:59:02 +0530

    In this file we have taken all the similar work and put them in a loop

---
+++ sum_in_a_range/sum_in_given_range_exp4.js
@@ -0,0 +1,10 @@
+let input = 5; 
+let sum = 0;
+let iterator = 1 ;
+let addingValue = 0;
+
+for( iterator ; iterator <= input ; iterator = iterator + 1 ){
+    addingValue = iterator;
+    sum = sum + addingValue;
+}
+console.log(sum);
