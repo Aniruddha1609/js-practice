18427810189a3a44c26116c072f4df920d5928c6
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-19 12:41:47 +0530

    Experimenting file for factorial for steps

---
+++ factorial/factorial_exp1.js
@@ -0,0 +1,25 @@
+const input = 5;
+let factorial = 1;
+let multiplier = input;
+
+// multiplying 5
+factorial = factorial * multiplier;
+multiplier = multiplier -1 ;
+
+// multiplying 4
+factorial = factorial * multiplier;
+multiplier = multiplier -1 ;
+
+// multiplying 3
+factorial = factorial * multiplier;
+multiplier = multiplier -1 ;
+
+// multiplying 2 
+factorial = factorial * multiplier;
+multiplier = multiplier -1 ;
+
+//multiplying 1
+factorial = factorial * multiplier;
+multiplier = multiplier -1 ;
+
+console.log(factorial)
