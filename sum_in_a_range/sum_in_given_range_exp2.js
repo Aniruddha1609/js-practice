9fa0adfbaf3e5fb9032ab163c5ed36289e76c31e
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-19 13:55:08 +0530

    sum_in_given_range_exp2.js file has a repeating work done multiple times

---
+++ sum_in_a_range/sum_in_given_range_exp2.js
@@ -0,0 +1,44 @@
+const input = 7;
+let sum = 0;
+let addingValue=input;
+
+// adding 9
+sum = sum + addingValue ;
+addingValue = addingValue - 1 ;
+
+// adding 8
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+//adding 7
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+// adding 6
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+// adding 5
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+// adding 4
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+//adding 3
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+// adding 2
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+// adding 1
+sum = sum + addingValue;
+addingValue = addingValue - 1 ;
+
+console.log(sum);
+
+
+
