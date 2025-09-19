4b9975328e53f1dcea167130cd9fc90342bf4c7b
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-19 23:22:51 +0530

    Basic logic and understanding of first_10_odd_numbers_exp1.js

---
+++ first_ten_odd_numbers_from_a_starting_value/first_10_odd_numbers_exp1.js
@@ -0,0 +1,51 @@
+let currentValue = 5;
+let isOdd = (currentValue % 2 !== 0) ? true : false ;
+let firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
+
+isOdd = (currentValue % 2 !== 0) ? true : false ;
+firstOddNumber = (isOdd === true) ? currentValue : currentValue + 1
+console.log(firstOddNumber);
+currentValue = currentValue + 2;
