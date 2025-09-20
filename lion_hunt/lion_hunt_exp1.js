f83b665aad540b61f8ba38a72ac020d0c3a2f2f2
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-20 12:29:07 +0530

    This file has only finding the lions positions

---
+++ lion_hunt/lion_hunt_exp1.js
@@ -0,0 +1,8 @@
+const animalField = "L     Z";
+
+for (let lionFinder = 0; lionFinder < animalField.length; lionFinder++) {
+  if (animalField[lionFinder] === "L") {
+    console.log(lionFinder);
+    
+  }
+}  
